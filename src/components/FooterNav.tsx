0ae7cac1d5cc1b901c452f75acbdd6d0dc195aa9
Pablo del Barrio <pablodbb@gmail.com>
2023-08-11 16:17:50 +0200

    fixed styles y footernav

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -1,15 +1,30 @@
 import { Link } from "react-router-dom";
 import { Cloud, Disc } from "./Icons";
+import { useState } from "react";
 
 export default function FooterNav() {
+  const [isDiscClicked, setIsDiscClicked] = useState<boolean>(true);
+  const [isCloudClicked, setIsCloudClicked] = useState<boolean>(false);
   return (
     <footer className="fixed bottom-0 bg-white border-[1px] border-plastic-blue-light flex justify-center pt-[16px] pb-[32px] w-full max-w-screen-sm">
       <div className="flex gap-20">
-        <Link to="/">
-          <Disc />
+        <Link
+          to="/"
+          onClick={() => {
+            setIsDiscClicked(true);
+            setIsCloudClicked(false);
+          }}
+        >
+          <Disc isClicked={isDiscClicked} />
         </Link>
-        <Link to="/weather">
-          <Cloud />
+        <Link
+          to="/weather"
+          onClick={() => {
+            setIsCloudClicked(true);
+            setIsDiscClicked(false);
+          }}
+        >
+          <Cloud isClicked={isCloudClicked} />
         </Link>
       </div>
     </footer>
--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,39 +1,53 @@
-export const Disc = () => {
+interface IconProps {
+  isClicked: boolean;
+}
+
+export const Disc = ({ isClicked }: IconProps) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="feather feather-disc text-plastic-blue"
-    >
-      <circle cx="12" cy="12" r="10"></circle>
-      <circle cx="12" cy="12" r="3"></circle>
-    </svg>
+    <div className="relative">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className={isClicked ? "text-plastic-blue" : "text-plastic-blue-light"}
+      >
+        <circle cx="12" cy="12" r="10"></circle>
+        <circle cx="12" cy="12" r="3"></circle>
+      </svg>
+      {isClicked && (
+        <div className="absolute w-[5px] h-[5px] bg-plastic-blue rounded-full bottom-[-8px] left-1/2 transform -translate-x-1/2"></div>
+      )}
+    </div>
   );
 };
 
-export const Cloud = () => {
+export const Cloud = ({ isClicked }: IconProps) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="feather feather-cloud text-plastic-blue"
-    >
-      <path d="M18 10h-1.26A8 8 0 1 0 9 20h9a5 5 0 0 0 0-10z"></path>
-    </svg>
+    <div className="relative">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className={isClicked ? "text-plastic-blue" : "text-plastic-blue-light"}
+      >
+        <path d="M18 10h-1.26A8 8 0 1 0 9 20h9a5 5 0 0 0 0-10z"></path>
+      </svg>
+      {isClicked && (
+        <div className="absolute w-[5px] h-[5px] bg-plastic-blue rounded-full bottom-[-8px] left-1/2 transform -translate-x-1/2"></div>
+      )}
+    </div>
   );
 };
 
